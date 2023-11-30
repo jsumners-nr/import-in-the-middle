57fddf68dfd16808749a29a810171e239dac7fe6
James Sumners <jsumners@newrelic.com>
2023-11-30 14:14:14 -0500

    less wut

--- test/hook/cjs-module-exports.mjs
+++ test/hook/cjs-module-exports.mjs
@@ -3,11 +3,11 @@ import Hook from '../../index.js'
 
 Hook((exports, name) => {
   if (/module-exports\.js$/.test(name) === false) return
-  exports.foo = 'foo-hooked'
+  exports.default.foo += '-hooked'
 })
 
 import * as mod1 from '../fixtures/module-exports.js'
-strictEqual(mod1.foo, 'foo-hooked')
+strictEqual(mod1.default.foo, 'foo-hooked')
 
 import mod2 from '../fixtures/module-exports.js'
 strictEqual(mod2.foo, 'foo-hooked')
